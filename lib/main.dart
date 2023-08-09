f85a28a9fdd0d7c943bc355f99ff237e007fc2fc
Tewodros Nibret Adal <tewodrosnibretadal2012@gamil.com>
2023-08-09 13:00:39 +0300

    slider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
@@ -21,25 +19,104 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MySlider extends StatelessWidget {
-     const MySlider({super.key});
+class MySlider extends StatefulWidget {
+  const MySlider({super.key});
 
-@override
-  Widget build(context){
-     return CarouselSlider(
-    items: [1, 2, 3, 4, 5]
-        .map((i) => Container(
-              width: MediaQuery.of(context).size.width,
-              margin: const EdgeInsets.symmetric(horizontal: 5),
-              decoration: const BoxDecoration(
-                color: Colors.purple,
-              ),
-              child:  Center(child:Text("number $i")),
-            ))
-        .toList(),
-    options: CarouselOptions(height: 24),
-  );
+  @override
+  _MySliderState createState() => _MySliderState();
+}
 
+class _MySliderState extends State<MySlider> {
+  int _currentSlide = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> carouselItems = [
+      Container(
+        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.symmetric(horizontal: 32),
+        decoration: const BoxDecoration(
+          color: Color.fromRGBO(107, 59, 225,1)
+        ),
+        child: Center(child: Text("Widget 1")),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.symmetric(horizontal: 32),
+        decoration: const BoxDecoration(
+          color: Color.fromRGBO(107, 59, 225,1),
+        ),
+        child: Center(child: Text("Widget 2")),
+      ),
+      Container(
+        width: MediaQuery.of(context).size.width,
+        margin: const EdgeInsets.symmetric(horizontal: 32),
+        decoration: const BoxDecoration(
+          color:  Color.fromRGBO(107, 59, 225,1),
+        ),
+        child: Center(child: Text("Widget 3")),
+      ),
+      // Add more widgets here
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                CarouselSlider(
+                  items: carouselItems,
+                  options: CarouselOptions(
+                    height: 150,
+                    viewportFraction: 1.0,
+                    enlargeCenterPage: true,
+                    onPageChanged: (index, reason) {
+                      setState(() {
+                        _currentSlide = index;
+                      });
+                    },
+                  ),
+                ),
+                Positioned(
+                  top: 12.0,
+                  right: 40.0,
+                  child: CarouselStatus(
+                    itemCount: carouselItems.length,
+                    currentSlide: _currentSlide,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
- 
+}
+
+class CarouselStatus extends StatelessWidget {
+  final int itemCount;
+  final int currentSlide;
+
+  CarouselStatus({required this.itemCount, required this.currentSlide});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        for (int index = 0; index < itemCount; index++)
+          Container(
+            width: 8,
+            height: 8,
+            margin: const EdgeInsets.symmetric(horizontal: 4),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: currentSlide == index ? Color.fromARGB(255, 30, 69, 224) : Colors.white,
+            ),
+          ),
+      ],
+    );
+  }
 }
